6f44c62dc0d88b3cc70cab47bc6ed44e49cb62e4
kiylx <33772264+Knightwood@users.noreply.github.com>
2023-06-25 19:40:26 +0800

    update

--- libx/src/main/java/com/kiylx/libx/http/kotlin/common/MultiRetrofitHolder.kt
+++ libx/src/main/java/com/kiylx/libx/http/kotlin/common/MultiRetrofitHolder.kt
@@ -61,8 +61,20 @@ class SingleServiceCreator private constructor(baseUrl: String) {
 /**
  * 持有okhttpclient和retrofit
  * 继承并重写方法，以实现自定义
+ * @param baseUrl baseurl
+ * @param json 自定义Json的配置
+ * ```
+ * prettyPrint = true //json格式化
+ * isLenient = true //宽松解析，json格式异常也可解析，如：{name:"小红",age:"18"} + Person(val name:String,val age:Int) ->Person("小红",18)
+ * ignoreUnknownKeys = true //忽略未知键，如{"name":"小红","age":"18"} ->Person(val name:String)
+ * coerceInputValues =  true //强制输入值，如果json属性与对象格式不符，则使用对象默认值，如：{"name":"小红","age":null} + Person(val name:String = "小绿"，val age:Int = 18) ->Person("小红",18)
+ * encodeDefaults =  true //编码默认值,默认情况下，默认值的属性不会参与序列化，通过设置encodeDefaults = true,可让默认属性参与序列化(可参考上述例子)
+ * explicitNulls =  true //序列化时是否忽略null
+ * allowStructuredMapKeys =  true //允许结构化映射(map的key可以使用对象)
+ * allowSpecialFloatingPointValues =  true //特殊浮点值：允许Double为NaN或无穷大
+```
  */
-open class Retrofit2Holder(val baseUrl: String) {
+open class Retrofit2Holder(private val baseUrl: String, private val json: Json = Json) {
     val mOkHttpClient: OkHttpClient by lazy { createOkHttpClient() }
     val mRetrofit: Retrofit by lazy { createRetrofit(mOkHttpClient) }
     private val contentType = "application/json".toMediaType()
@@ -71,7 +83,7 @@ open class Retrofit2Holder(val baseUrl: String) {
     open fun createRetrofit(okHttpClient: OkHttpClient): Retrofit {
         val builder = Retrofit.Builder()
             .baseUrl(baseUrl)
-            .addConverterFactory(Json.asConverterFactory(contentType))
+            .addConverterFactory(json.asConverterFactory(contentType))
             .client(okHttpClient)
         return builder.build()
     }
